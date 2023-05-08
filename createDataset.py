722c3312e7d9768a229c84fb0f46420c445b9da5
TC <candice02110211@gmail.com>
2023-05-08 14:34:52 +0800

    add the label and check label funciton

--- createDataset.py
+++ createDataset.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def main():
+def buildDataset():
     image = cv2.imread("stave.png")
     image = cv2.resize(image, (560, 800))
 
@@ -19,14 +20,110 @@ def main():
                 datasetX.append(crop_img)
         y += line_space
 
-    print(len(datasetX))
+    datasetSize = len(datasetX)
+    print("size of dataset X =", datasetSize)
+    np.save('datasetX_1', datasetX[:len(datasetX) // 3])
+    np.save('datasetX_2', datasetX[len(datasetX) // 3 : 2 * len(datasetX) // 3])
+    np.save('datasetX_3', datasetX[2 * len(datasetX) // 3:])
+    np.save('datasetY_1', np.full(datasetSize // 3, -1, dtype=int))
+    np.save('datasetY_2', np.full(datasetSize // 3, -1, dtype=int))
+    np.save('datasetY_3', np.full(datasetSize // 3, -1, dtype=int))
 
-    '''for i in range(-5, 5):
-        cv2.imshow("Output Image", datasetX[16000+i])
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()'''
+def label_1():
+    datasetX_1 = np.load("datasetX_1.npy")
+    datasetY_1 = np.load("datasetY_1.npy")
+
+    start = np.where(datasetY_1==-1)[0][0]
+    
+    for i in range(start, len(datasetX_1)):
+        plt.imshow(datasetX_1[i])
+        plt.title(f"image {i}")
+        plt.show()
+        tmp = input(f"label: ")
+        if tmp == 'q':
+            np.save('datasetY_1', datasetY_1)
+            break
+        else:
+            datasetY_1[i] = int(tmp)
+
+
+def label_2():
+    datasetX_2 = np.load("datasetX_2.npy")
+    datasetY_2 = np.load("datasetY_2.npy")
+
+    start = np.where(datasetY_2==-1)[0][0]
+    
+    for i in range(start, len(datasetX_2)):
+        plt.imshow(datasetX_2[i])
+        plt.title(f"image {i}")
+        plt.show()
+        tmp = input(f"label: ")
+        if tmp == 'q':
+            np.save('datasetY_2', datasetY_2)
+            break
+        else:
+            datasetY_2[i] = int(tmp)
+
+
+def label_3():
+    datasetX_3 = np.load("datasetX_3.npy")
+    datasetY_3 = np.load("datasetY_3.npy")
+
+    start = np.where(datasetY_3==-1)[0][0]
+    
+    for i in range(start, len(datasetX_3)):
+        plt.imshow(datasetX_3[i])
+        plt.title(f"image {i}")
+        plt.show()
+        tmp = input(f"label: ")
+        if tmp == 'q':
+            np.save('datasetY_3', datasetY_3)
+            break
+        else:
+            datasetY_3[i] = int(tmp)
+
+
+def checkLabel_1():
+    datasetX_1 = np.load("datasetX_1.npy")
+    datasetY_1 = np.load("datasetY_1.npy")
+    for i in range(0, len(datasetX_1), 10):
+        plt.figure(figsize=(10, 5))
+        for j in range(10):
+            plt.subplot(1, 10, j+1)
+            plt.imshow(datasetX_1[i+j])
+            plt.axis(False)
+            plt.title(f"{i+j}: {datasetY_1[i+j]}")
+        plt.show()
+
+
+def checkLabel_2():
+    datasetX_2 = np.load("datasetX_2.npy")
+    datasetY_2 = np.load("datasetY_2.npy")
+    for i in range(0, len(datasetX_2), 10):
+        plt.figure(figsize=(10, 5))
+        for j in range(10):
+            plt.subplot(1, 10, j+1)
+            plt.imshow(datasetX_2[i+j])
+            plt.axis(False)
+            plt.title(f"{i+j}: {datasetY_2[i+j]}")
+        plt.show()
+
+
+def checkLabel_3():
+    datasetX_3 = np.load("datasetX_3.npy")
+    datasetY_3 = np.load("datasetY_3.npy")
+    for i in range(0, len(datasetX_3), 10):
+        plt.figure(figsize=(10, 5))
+        for j in range(10):
+            plt.subplot(1, 10, j+1)
+            plt.imshow(datasetX_3[i+j])
+            plt.axis(False)
+            plt.title(f"{i+j}: {datasetY_3[i+j]}")
+        plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    buildDataset() # WARNING: don't run this function or you will reset all the labels
+    # label_1()
+    # checkLabel_1()
