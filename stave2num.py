959904453bfa43edaeed76d19d13f8837211cd69
TC <candice02110211@gmail.com>
2023-05-31 15:43:35 +0800

    add the testing image and result image

--- stave2num.py
+++ stave2num.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 import gzip
 
-def main(input_file, model):
+def main(input_file, output_prefix, model):
     image = cv2.imread(input_file)
     image = cv2.resize(image, (560, 800))
     output_image = image.copy()
@@ -13,7 +13,7 @@ def main(input_file, model):
     line_space = 106
     w = 10
     h = 45
-    for _ in range(4):
+    for _ in range(2):
         dx = 0
         while dx < 450:
             crop_img = image[y:y+h, x+dx:x+dx+w][:, :, 0]
@@ -22,31 +22,29 @@ def main(input_file, model):
                 dx += 3
             else:
                 cv2.putText(output_image, str(pred % 7 if (pred % 7) != 0 else 7), \
-                            (int(x+dx+w/2), int(y+h+4)), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.3, (255, 0, 0))
+                            (int(x+dx+w/2), int(y+h+4)), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (255, 0, 0))
                 if pred >= 8:
                     cv2.putText(output_image, ".", \
-                            (int(x+dx+w/2+2), int(y+h-4)), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.3, (255, 0, 0))
+                            (int(x+dx+w/2+2), int(y+h-4)), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (255, 0, 0))
                 if pred == 15:
                     cv2.putText(output_image, ".", \
-                            (int(x+dx+w/2+2), int(y+h-7)), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.3, (255, 0, 0))
+                            (int(x+dx+w/2+2), int(y+h-7)), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (255, 0, 0))
                 dx += 9
         y += line_space
     
-    cv2.imshow("Output Image", output_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.imwrite("output_" + input_file, output_image)
+    print("finish")
+    cv2.imwrite(output_prefix + input_file, output_image)
 
 if __name__ == '__main__':
-    input_file = "stave.png"
+    input_file = "bee.png"
     with gzip.open('OneVsOneModel.pgz', 'r') as f:
         OneVsOneModel = pickle.load(f)
-        main(input_file, OneVsOneModel)
+        main(input_file, "OneVsOne_", OneVsOneModel)
 
-    '''with gzip.open('OneVsRestModel.pgz', 'r') as f:
+    with gzip.open('OneVsRestModel.pgz', 'r') as f:
         OneVsRestModel = pickle.load(f)
-        main(input_file, OneVsRestModel)'''
+        main(input_file, "OneVsRest_", OneVsRestModel)
     
-    '''with gzip.open('KNeighborsModel.pgz', 'r') as f:
+    with gzip.open('KNeighborsModel.pgz', 'r') as f:
         KNeighborsModel = pickle.load(f)
-        main(input_file, KNeighborsModel)'''
+        main(input_file, "KNeighbors_", KNeighborsModel)
--- train.py
+++ train.py
@@ -9,37 +9,40 @@ import pickle
 import gzip
 
 def trainingOneVsOneModel(X_train, X_test, y_train, y_test):
-    start_time = time()
     OneVsOneModel = OneVsOneClassifier(SVC())
-    y_pred = OneVsOneModel.fit(X_train, y_train).predict(X_test)
+    OneVsOneModel.fit(X_train, y_train)
+    start_time = time()
+    y_pred = OneVsOneModel.predict(X_test)
     end_time = time()
     print("-----One Vs One Model-----")
-    print("Number of mislabeled points out of a total %d points : %d" % (len(X_test), (y_test != y_pred).sum()))
-    print("Running time = %.2f s" % (end_time - start_time))
+    print(f"Error rate: {((y_test != y_pred).sum() / len(X_test) * 100): .2f}%")
+    print(f"Testing time: {(end_time - start_time): .2f} s")
     
     with gzip.GzipFile('OneVsOneModel.pgz', 'w') as f:
         pickle.dump(OneVsOneModel, f)
 
 def trainingOneVsRestModel(X_train, X_test, y_train, y_test):
-    start_time = time()
     OneVsRestModel = OneVsRestClassifier(SVC())
-    y_pred = OneVsRestModel.fit(X_train, y_train).predict(X_test)
+    OneVsRestModel.fit(X_train, y_train)
+    start_time = time()
+    y_pred = OneVsRestModel.predict(X_test)
     end_time = time()
     print("-----One Vs Rest Model-----")
-    print("Number of mislabeled points out of a total %d points : %d" % (len(X_test), (y_test != y_pred).sum()))
-    print("Running time = %.2f s" % (end_time - start_time))
+    print(f"Error rate: {((y_test != y_pred).sum() / len(X_test) * 100): .2f}%")
+    print(f"Testing time: {(end_time - start_time): .2f} s")
 
     with gzip.GzipFile('OneVsRestModel.pgz', 'w') as f:
         pickle.dump(OneVsRestModel, f)
 
 def trainingKNeighborsModel(X_train, X_test, y_train, y_test):
-    start_time = time()
     KNeighborsModel = KNeighborsClassifier(n_neighbors=3)
-    y_pred = KNeighborsModel.fit(X_train, y_train).predict(X_test)
+    KNeighborsModel.fit(X_train, y_train)
+    start_time = time()
+    y_pred = KNeighborsModel.predict(X_test)
     end_time = time()
     print("-----K Neighbors Model-----")
-    print("Number of mislabeled points out of a total %d points : %d" % (len(X_test), (y_test != y_pred).sum()))
-    print("Running time = %.2f s" % (end_time - start_time))
+    print(f"Error rate: {((y_test != y_pred).sum() / len(X_test) * 100): .2f}%")
+    print(f"Testing time: {(end_time - start_time): .2f} s")
 
     with gzip.GzipFile('KNeighborsModel.pgz', 'w') as f:
         pickle.dump(KNeighborsModel, f)
@@ -57,6 +60,9 @@ if __name__ == '__main__':
     y_2 = np.load('datasetY_2.npy')
     y_3 = np.load('datasetY_3.npy')
     y = np.concatenate((y_1, y_2, y_3))
+    for i in range(16):
+        print(f"sum of label {i} image: {np.count_nonzero(y == i)}")
+    print()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0, stratify=y)
 
     trainingOneVsOneModel(X_train, X_test, y_train, y_test)
